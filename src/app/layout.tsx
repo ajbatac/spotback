35ba66e3094dfa9705635e5d81bd67b4c64adf4f
Rephrais Tidee <rephrais@gmail.com>
2025-07-10 20:04:35 +0000

    NOT WORKING NOT WORKING NOT WORKING NOT WORKING NOT WORKING NOT WORKING

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { AuthProvider } from '@/context/auth-context';
-import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'SpotBack - Backup Your Spotify Playlist',
@@ -22,19 +21,6 @@ export default function RootLayout({
           {children}
         </AuthProvider>
         <Toaster />
-        <Script
-          strategy="lazyOnload"
-          src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js"
-          data-name="bmc-button"
-          data-slug="emailsig"
-          data-color="#FFDD00"
-          data-emoji="🍕"
-          data-font="Cookie"
-          data-text="Buy me a pizza?"
-          data-outline-color="#000000"
-          data-font-color="#000000"
-          data-coffee-color="#ffffff"
-        />
       </body>
     </html>
   );
