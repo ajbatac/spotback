2b2a7941337503a0f65adfc617cff938c0694290
Rephrais Tidee <rephrais@gmail.com>
2025-07-12 18:20:59 +0000

    we lost the button hover again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,9 +50,9 @@ function LoginPage() {
   }, []);
 
   return (
-    <>
-      <main className="flex flex-col items-center p-8 pt-20">
-        <div className="flex flex-col items-center justify-center text-center space-y-6 max-w-lg mx-auto">
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow flex flex-col items-center justify-center p-8 text-center">
+        <div className="flex flex-col items-center space-y-6 max-w-lg mx-auto">
           <div className="bg-primary/20 p-4 rounded-full">
             <Music className="w-12 h-12 text-primary" />
           </div>
@@ -83,7 +83,7 @@ function LoginPage() {
         </div>
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
 
